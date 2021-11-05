e4978dabc96155975dfd7cb67090436c1c0029ed
Florin Lazar <florin.lazar.it2014@gmail.com>
2021-11-05 10:25:00 +0200

    Iterate letters in normal order and in reverse order

--- LoopStatementsFor/LoopStatementsFor/Program.cs
+++ LoopStatementsFor/LoopStatementsFor/Program.cs
@@ -6,7 +6,22 @@ namespace LoopStatementsFor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Please enter a text=");
+            string text = Console.ReadLine();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                Console.Write(text[i]);
+            }
+
+            Console.WriteLine();
+
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                Console.Write(text[i]);
+            }
+
+            Console.WriteLine();
         }
     }
 }
